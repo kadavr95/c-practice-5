0a1332499ec631aea7199ed15324a0f1260e097b
Yaskovich Dmitry <kadavr95@outlook.com>
2012-10-27 01:49:00 +0300

    fixes

--- SR5V7.c
+++ SR5V7.c
@@ -4,13 +4,19 @@
 
 int main(void){
   double s=0.0,scor=0.0,cor=0.0,fcor,f,s2=0.0;
-  int n1=1,n2=5,n3=10,i;
+  int n1=100,n2=500,n3=1000,i;
   printf("Lab 5. Variant 3\n\n");
   for (i=1; i <= n3 ; i++) {
+	f=((2*i-1)/(pow(2,i)));
+	fcor=f-cor;
+	scor=s+fcor;
+	cor=(scor-s)-fcor;
+	s=scor;
+	s2=s2+f;
 	if ((i==n1) || (i==n2) || (i==n3)) {
 	  printf("Number:       %d\n",i);
-	  printf("No correction:%.30e\n",s2);
-	  printf("Correction:   %.30e\n",s);
+	  printf("No correction:%.50e\n",s2);
+	  printf("Correction:   %.50e\n",s);
 	  if ((s2-s)>0)
 		printf("Comparison:   Sum with no correction is bigger than sum with correction\n");
 	  if ((s2-s)<0) {
@@ -19,14 +25,8 @@ int main(void){
 	  if ((s2-s)==0) {
 		printf("Comparison:   Sum with no correction is equal to sum with correction\n");
 	  }
-	  printf("Difference:   %.30e\n\n",fabs(s2-s));
+	  printf("Difference:   %.50e\n\n",fabs(s2-s));
 	}
-	f=((2*i-1)/(pow(2,i)));
-	fcor=f-cor;
-	scor=s+fcor;
-	cor=(scor-s)-fcor;
-	s=scor;
-	s2=s2+f;
   }
   printf("Dimini Inc.\n");
   printf("For progressive future\n");
