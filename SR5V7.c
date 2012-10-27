c6d0549bcf2f577ba3a56d27c236fdf6dec68d07
Yaskovich Dmitry <kadavr95@outlook.com>
2012-10-28 00:45:00 +0300

    added comments

--- SR5V7.c
+++ SR5V7.c
@@ -1,33 +1,30 @@
-#include <stdio.h>
-#include <limits.h>
+#include <stdio.h>                                                              //подключение библиотек содержащих функции ввода, вывода и математических  функций
 #include <math.h>
-#include <float.h>
 
 int main(void){
-  long double s=0.0,scor=0.0,cor=0.0,fcor,f,s2=0.0,ss;
-  int n1=100,n2=500,n3=1000,i,ii=-1;
-  printf("Exercise 5. Variant 7\n\n");
-  for (i=1; i <= n3 ; i++) {
+  long double s=0.0,scor=0.0,cor=0.0,fcor,f;                                    //определение переменных и присваивание им необходимых значений
+  short int n1=60,n2=500,n3=1000,i,ii=-1;
+  printf("Yaskovich Dmitry (T01-01c). Exercise 5. Variant 7\n\n");              //Информация о программе
+  for (i=1; i <= n3 ; i++) {                                                    //цикл, проходящий по всем i в пределах заданных в условии
 	f=((2*i-1)/(pow(2,i)));
-	fcor=f-cor;
+	fcor=f-cor;                                                                 //суммирование по методу Кохена
 	scor=s+fcor;
 	cor=(scor-s)-fcor;
 	s=scor;
-	s2=s2+f;
-	if ((i==n1) || (i==n2) || (i==n3)) {
-	  printf("Number:       %d\n",i);
+	if ((i==n1) || (i==n2) || (i==n3)) {                                        //условие вывода результатов
+	  printf("Number:       %d\n",i);                                           //вывод результатов
 	  printf("Sum:          %.30Lf\n\n",s);
 
 	}
-	if (((3-s2)<0.001)&&(ii<0) ){
-	  ii=i;
+	if (((3-s)<0.001)&&(ii<0) ){                                                //проверка достижения необходимого значения
+	  ii=i;                                                                     //сохранение наименьшего значения n
 	}
   }
-  printf("Minimal No:   %d\n\n",ii);
-  printf("Dimini Inc.\n");
+  printf("Minimal N:    %d\n\n",ii);                                            //вывод минимального n
+  printf("Dimini Inc.\n");                                                      //Авторские права
   printf("For progressive future\n");
   printf("(c)2009-2012\n");
-  fflush(stdin);
+  fflush(stdin);                                                                //ожидание нажатия на клавишу для завершения программы
   getchar();
   return 0;
 }
